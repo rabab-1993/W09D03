5aecff2fa787aab603379dbabcb0c84441f05401
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2021-12-08 02:17:11 +0300

    add login

--- Components/home /Home.js
+++ Components/home /Home.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 const Home = () => {
+  let navigate = useNavigate();
+  // Navigate function to login page
+  let toLogInPage = () => {
+    navigate("/login");
+  };
   return (
     <div className="home">
       <nav>
--- Components/login/LogIn.js
+++ Components/login/LogIn.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { logIn } from "../reducers/login";
 
-const LogIn = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Login = () => {
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
 
-export default LogIn
+  const [logUser, setLogUser] = useState({
+    email: "",
+    password: "",
+  });
+  let toTaskPage = () => {
+    navigate("/task");
+  };
+  const login = async () => {
+    try {
+      const result = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/user/login`,
+        logUser
+      );
+
+      const data = {
+        token: result.data.token,
+        user: result.data.result.user._id,
+      };
+      console.log(data);
+      dispatch(logIn(data));
+      toTaskPage();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        name="email"
+        id=""
+        placeholder="email"
+        onChange={(ev) => setLogUser({ ...logUser, email: ev.target.value })}
+      />
+      <input
+        type="password"
+        name="pass"
+        id=""
+        placeholder="password"
+        onChange={(ev) => setLogUser({ ...logUser, password: ev.target.value })}
+      />
+      <button onClick={login}>LogIn</button>
+    </div>
+  );
+};
+
+export default Login;
